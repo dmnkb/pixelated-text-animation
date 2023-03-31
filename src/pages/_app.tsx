fb724c352651bb771b6c9f6d5eca53ea5aeb239e
Dominik Borchert <dominik.borchert@lichtblick.de>
2023-03-31 21:59:19 +0200

    feat: :art: add font
    Add font.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import '@/styles/globals.css';
+import { Inter } from 'next/font/google';
 
 import type { AppProps } from 'next/app';
 
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
+
 const App = ({ Component, pageProps }: AppProps) => (
-  <>
+  <main className={`${inter.variable} font-sans`}>
     <Component {...pageProps} />
-  </>
+  </main>
 );
 
 // eslint-disable-next-line import/no-default-export
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
+export const content = ['./src/**/*.{js,ts,jsx,tsx}'];
+export const theme = {
+  extend: {
+    fontFamily: {
+      sans: ['var(--font-inter)', ...fontFamily.sans],
+    },
+    container: {
+      center: true,
+    },
   },
-  plugins: [],
 };
+export const plugins = [];
