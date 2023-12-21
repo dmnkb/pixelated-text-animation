44b1b385a7254a30b9a1ad9f2baee7bac6ebb253
Dominik Borchert <dev@borchert.me>
2023-12-21 17:45:46 +0100

    fix: remove style from _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,9 @@
 import '@/styles/globals.css';
-import { Inter } from 'next/font/google';
 
 import type { AppProps } from 'next/app';
 
-const inter = Inter({ subsets: ['latin'] });
-
 const App = ({ Component, pageProps }: AppProps) => (
   <>
-    <style global jsx>
-      {`
-        :root {
-          --font-font: ${inter.style.fontFamily};
-        }
-      `}
-    </style>
     <Component {...pageProps} />
   </>
 );
